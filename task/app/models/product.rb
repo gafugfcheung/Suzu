41126c347707dff93b815846d4e06f15530e3da0
Bosco Cheung <cheung.bosco.96@gmail.com>
2016-02-27 19:04:44 +0000

    Add more validation to price

--- task/app/models/product.rb
+++ task/app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, 
+                    numericality: { only_integer: true, 
+                                    greater_than_or_equal_to: 0 }
 end
