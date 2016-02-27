0e02d435c45b85743fd1e6e8269a26876c45c30c
Bosco Cheung <cheung.bosco.96@gmail.com>
2016-02-27 21:17:16 +0000

    Clean up duplication and add destroy action

--- task/app/controllers/products_controller.rb
+++ task/app/controllers/products_controller.rb
@@ -31,6 +31,13 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+
+    redirect_to root_path
+  end
+
   private
     def product_params
       params.require(:product).permit(:name, :description, :price)
